985f87bd2f3b285084bb3af78e5ae8e668f801d0
NeverBehave <gayhub@never.pet>
2021-02-09 14:51:55 -0500

    comment

--- hw1/main.c
+++ hw1/main.c
@@ -7,24 +7,27 @@
 #define WORD_BUFFER 128
 #define VALID_WORD_LENGTH 3
 
+/** 
+ * hash based on the ascii num
+ * at this moment, word has no \0 ends and that's why we need size
+ **/ 
 int hash(const char *word, int cache_size, int word_size) {
     int sum = 0;
     for (int i = 0; i < word_size; i++) {
-#ifdef DEBUG_STATEMENT
-        printf("%c", *(word + i));
-#endif
         sum += *(word + i);
     }
-#ifdef DEBUG_STATEMENT
-    printf(" => %d\n", sum);
-#endif
     return sum % cache_size;
 }
 
+/**
+ * this function take the cache and word_buffer,
+ * correctly set the end char and save to cache.
+ **/
 void set(char *word_buffer, int hash_num, int index, char **cache) {
     // end of word
     *(word_buffer + index) = '\0';
-    // make up space
+    // if current space is empty, we put in directly
+    // or we reallocate the cache slot for our new word
     if (*(cache + hash_num) == NULL) {
         *(cache + hash_num) = calloc(sizeof(char), index + 1);
         printf("Word \"%s\" ==> %d (calloc)\n", word_buffer, hash_num);
@@ -46,14 +49,15 @@ int main(int argc, char **argv) {
     char *cache_size_char = *(argv + 1);
     int cache_size = strtol(cache_size_char, NULL, 10);
     if (cache_size <= 0) {
-        // 0 will return if conversion failed.
-        // But in this case, 0 is also not a valid cache_size.
+        // 0 will be returned if conversion has failed
+        // but in this case, 0 is also not a valid cache_size
         fprintf(stderr, "Error: invalid cache size entered. %s", cache_size_char);
         return EXIT_FAILURE;
     }
     char **cache = calloc(sizeof(char *), cache_size);
     char *word_buffer = calloc(sizeof(char), WORD_BUFFER);
 
+    // file args should start from 2
     int num_files = 2;
     while (num_files < argc) {
         int fd = open(*(argv + num_files), O_RDONLY);
@@ -66,6 +70,7 @@ int main(int argc, char **argv) {
         char letter;
         int index = 0;
         while (read(fd, &letter, 1) > 0) {
+            // words will only contains letters and should longer than 3
             if ((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z')) {
                 *(word_buffer + index) = letter;
                 index++;
@@ -74,12 +79,14 @@ int main(int argc, char **argv) {
                     set(word_buffer, hash(word_buffer, cache_size, index), index, cache);
                 }
 
-                // For next round
+                // reset for next round
+                // word_buffer will be override and is on stack
+                // so we don't need to reset it
                 index = 0;
             }
         }
 
-        // We may have no separator at last
+        // we may have no separator at last
         if (index >= VALID_WORD_LENGTH) {
             set(word_buffer, hash(word_buffer, cache_size, index), index, cache);
         }
@@ -88,6 +95,7 @@ int main(int argc, char **argv) {
         num_files++;
     }
 
+    // print cache
     for (int i = 0; i < cache_size; i++) {
         if (*(cache + i) != NULL) {
             printf("Cache index %d ==> \"%s\"\n", i, *(cache + i));
